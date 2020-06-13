4340bc72c91dc2cf230a203271f4c2da636965c2
Hariom Yadav <hariom.yadav@broadcom.com>
2020-06-13 12:12:23 +0530

    Add movie component and import and call component in App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-
+import Movies from "./components/movies";//1
 function App() {
   return (
    <main className="container">
-     <h1>Hello</h1>
+     <Movies />
    </main>
   );
 }
---
+++ src/components/movies.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+class Movies extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>Movies Component</h1> );
+    }
+}
+ 
+export default Movies;
